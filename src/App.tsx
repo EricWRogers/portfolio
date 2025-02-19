b48455ed1047c28f75c195b6a7828ddda0835485
EricWRogers <ericwilliamrogers@gmail.com>
2025-02-19 14:23:25 -0600

    Video and Bar

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,9 @@ import { IoLogoYoutube } from "react-icons/io";
 import { FaSquareXTwitter } from "react-icons/fa6";
 import { FaGithub } from "react-icons/fa";
 import { FaSteam } from "react-icons/fa";
+import { FaLinkedin } from "react-icons/fa";
 import Card from './Card.tsx';
+import myVideo from '/FHTrialer.mp4';
 
 
 
@@ -15,6 +17,7 @@ function App() {
 
   return (
     <>
+      <h1>Eric W. Rogers</h1>
       <div>
       <img src={profilePic} className="profile" alt="yourgeekeric logo" />
         <br />
@@ -30,8 +33,16 @@ function App() {
         <a href='https://store.steampowered.com/search/?developer=SuperPupStudio' target="_blank">
           <FaSteam size={48} className='social'/>
         </a>
+        <a href='https://www.linkedin.com/in/eric-rogers-b12874163/'>
+          <FaLinkedin size={48} className='social'/>
+        </a>
       </div>
-      <h1>Eric W. Rogers</h1>
+      <video width="640" height="360" controls autoPlay muted loop className='videoPlayer'>
+        <source src={myVideo} type="video/mp4" className='videoPlayer'/>
+        Your browser does not support the video tag.
+      </video>
+      <hr/>
+      <h2>Projects</h2>
       <Card
         imageSrc={StopTheSlimesPic}
         link="https://store.steampowered.com/app/2659530/Stop_The_Slimes/"
